b4964ecc621a62aceac3da1e86e06339c7d2b3df
SaloniPardeshi1412 <saloni.p@binyuga.com>
2024-02-15 16:43:38 +0530

    Lecture 1 completed.

---
+++ lib/app/data/services/repository.dart
@@ -0,0 +1,11 @@
+import 'package:todo_app_getx/app/data/providers/provider.dart';
+
+import '../models/task.dart';
+
+class TaskRepository{
+  TaskProvider taskProvider;
+  TaskRepository({required this.taskProvider});
+
+  List<Task> readTasks() => taskProvider.readTasks();
+  void writeTasks(List<Task> tasks) => taskProvider.writeTasks(tasks);
+}
